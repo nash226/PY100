798372b0f2d588e0f6a1e7f05b8a4ad0b54cc68e
Nazeer S <63686800+nash226@users.noreply.github.com>
2025-06-21 23:37:43 -0400

    Delete intro_python_exercises/control_flow directory

--- intro_python_exercises/control_flow/all_caps.py
+++
@@ -1,8 +0,0 @@
-def all_caps(my_string):
-
-    if len(my_string) > 10:
-        print(my_string.upper())
-    else:
-        print(my_string)
-
-all_caps(input('type some words: '))
--- intro_python_exercises/control_flow/even_or_odd.py
+++
@@ -1,8 +0,0 @@
-def even_or_odd(number):
-
-    if number % 2 == 0:
-        print('even')
-    else:
-        print('odd')
-
-even_or_odd(int(input("pick a number: ")))
